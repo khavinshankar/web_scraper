ad64fd81e3d4e6d95d9373d92b9fc8083d42755c
khavinshankar <khavinshankar@gmail.com>
2020-01-18 22:59:36 +0530

    added optional features

--- index.js
+++ index.js
@@ -3,71 +3,109 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 const url = "https://www.screener.in/company/PFC/consolidated/";
+options = {
+  include: [],
+  exclude: [],
+  format: "json", // json and csv
+  file: "single", // single, multiple and both
+  reverse: false
+};
+
 main(url);
 
 async function main(url) {
   const document = await request.get(url);
   const $ = await cheerio.load(document);
-  const data = scrape($);
-
-  //save_as_json(data);
-  save_as_csv(data);
+  scrape($, options);
 }
 
-function scrape($) {
+function scrape($, options) {
   const pg = [];
   const tables = $("table");
   tables.each((i, table) => {
-    let tbl = [];
-    $(table)
-      .find("tr")
-      .each((j, row) => {
-        let rw = [];
-        $(row)
-          .find("td, th")
-          .each((k, element) => {
-            rw.push(
-              $(element)
-                .text()
-                .trim()
-            );
-          });
-        tbl.push(rw);
-      });
-    pg.push(tbl);
+    if (
+      (options.include.includes(i + 1) || options.include.length === 0) &&
+      !options.exclude.includes(i + 1)
+    ) {
+      let tbl = [];
+      $(table)
+        .find("tr")
+        .each((j, row) => {
+          let rw = [];
+          $(row)
+            .find("td, th")
+            .each((k, element) => {
+              rw.push(
+                $(element)
+                  .text()
+                  .trim()
+              );
+            });
+          tbl.push(rw);
+        });
+
+      if (options.reverse) {
+        tbl = transpose(tbl);
+      }
+
+      pg.push(tbl);
+    }
   });
 
-  return pg;
+  if (options.format === "csv") {
+    save_as_csv(pg, options.file);
+  } else {
+    save_as_json(pg, options.file);
+  }
 }
 
-function save_as_json(data) {
+function save_as_json(data, file) {
   const page = {};
-
   for (let i = 0; i < data.length; i++) {
     let table = {};
     for (let j = 0; j < data[i].length; j++) {
       table[`row${j + 1}`] = data[i][j];
     }
+    if (file !== "single") {
+      fs.writeFileSync(
+        `./data/tables/table${i + 1}.json`,
+        JSON.stringify(table)
+      );
+    }
+
     page[`table${i + 1}`] = table;
   }
-
-  fs.writeFileSync("./data/page.json", JSON.stringify(page));
+  if (file !== "multiple") {
+    fs.writeFileSync(`./data/page.json`, JSON.stringify(page));
+  }
 }
 
-function save_as_csv(data) {
-  let string = "";
+function save_as_csv(data, file) {
+  let page = "";
 
   for (let i = 0; i < data.length; i++) {
-    string = string.concat(`table${i + 1}\n`);
-
+    page = page.concat(`table${i + 1}\n`);
+    let string = "";
     for (let j = 0; j < data[i].length; j++) {
       for (let k = 0; k < data[i][j].length; k++) {
         string = string.concat(`"${data[i][j][k]}",`);
       }
       string = string.concat(`\n`);
     }
-    string = string.concat(`\n`);
+    if (file !== "single") {
+      fs.writeFileSync(`./data/tables/table${i + 1}.csv`, string);
+    }
+    page = page.concat(`${string}\n`);
   }
+  if (file !== "multiple") {
+    fs.writeFileSync(`./data/page.csv`, page);
+  }
+}
 
-  fs.writeFileSync("./data/page.csv", string);
+function transpose(arr) {
+  return Object.keys(arr[0]).map(column => {
+    return arr.map(row => {
+      return row[column];
+    });
+  });
 }
