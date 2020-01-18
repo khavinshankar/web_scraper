13328eece74e25edeab00360c6afc882406de22b
khavinshankar <khavinshankar@gmail.com>
2020-01-18 15:14:15 +0530

    csv format added

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ async function main(url) {
   const document = await request.get(url);
   const $ = await cheerio.load(document);
   const data = scrape($);
-  save_as_json(data);
+
+  //save_as_json(data);
+  save_as_csv(data);
 }
 
 function scrape($) {
@@ -24,7 +26,11 @@ function scrape($) {
         $(row)
           .find("td, th")
           .each((k, element) => {
-            rw.push($(element).text());
+            rw.push(
+              $(element)
+                .text()
+                .trim()
+            );
           });
         tbl.push(rw);
       });
@@ -46,4 +52,22 @@ function save_as_json(data) {
   }
 
   fs.writeFileSync("./data/page.json", JSON.stringify(page));
+}
+
+function save_as_csv(data) {
+  let string = "";
+
+  for (let i = 0; i < data.length; i++) {
+    string = string.concat(`table${i + 1}\n`);
+
+    for (let j = 0; j < data[i].length; j++) {
+      for (let k = 0; k < data[i][j].length; k++) {
+        string = string.concat(`"${data[i][j][k]}",`);
+      }
+      string = string.concat(`\n`);
+    }
+    string = string.concat(`\n`);
+  }
+
+  fs.writeFileSync("./data/page.csv", string);
 }
